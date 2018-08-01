b4c45e63d9b6d10ff9c5b5706181caeecf3d26c2
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-08-01 15:36:08 +0100

    initial commit

---
+++ models/traveller.js
@@ -0,0 +1,34 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+return this.journeys.map(journey => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+return this.journeys.map(journey => journey.transport);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
